8afb16e7dfda082dc22bbad223dec7c7c3c95254
Lorenzo Manacorda <lorenzo@mailbox.org>
2017-03-31 10:49:27 +0200

    check if tests are run

---
+++ false_positive_test.go
@@ -0,0 +1,11 @@
+package certificatetpr
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidComponent(t *testing.T) {
+	assert.Equal(t, true, false, "This should fail")
+}
